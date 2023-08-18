5ff5a65bd6ff57b940b76e6383ae6e101372f105
James Burt <38007746+Scoce@users.noreply.github.com>
2023-08-18 15:29:00 -0500

    Adds completion to 03 and removes skips in test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+let newarray = [];
 
-};
+const reverseString = function(stringToReverse) {
+    const result = [];
+    for (i=0; i < stringToReverse.length; i++) {
+    result[i] = stringToReverse.slice(i, i+1);
+}
+    reversedResult =result.reverse();    
+return reversedResult.join("");
+}
+
+console.log(reverseString("Hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
