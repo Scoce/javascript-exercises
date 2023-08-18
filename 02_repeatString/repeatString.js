260ae3de2a07232c5b75f8744376d6952e7b937a
James Burt <38007746+Scoce@users.noreply.github.com>
2023-08-18 12:27:59 -0500

    Adds completion to 02 exercize in repeatString and uncomments the .skip tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
-};
+function repeatString(word, num) {
+    let string ="";
+   
+    for (let i = 0; i < num; i++) {
+        
+        string+=word;
+    } 
+    if (num < 0) {
+        string = "ERROR"
+    }
+    return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
---
+++ 02_repeatString/scratch.js
@@ -0,0 +1,6 @@
+string = "how";
+string2 = "far";
+
+string2+= string;
+
+console.log(string2);
